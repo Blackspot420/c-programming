de1d806505529ab6926ee0878e3bf2a8906c506b
Blackspot420 <ronnyyegon420@gmail.com>
2024-05-27 22:52:28 +0300

    Create Prompting user.cpp

---
+++ Prompting user.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+  int height;
+  double width;
+  float length;
+  int volume=height*width*length;
+  
+  printf("enter height");
+  printf("enter width");
+  printf("enter length");
+  
+   scanf("height %d");
+   scanf("width %f"); 
+   scanf("length %2f");
+
+    
+    
+    
+    
+    return 0;
+}
