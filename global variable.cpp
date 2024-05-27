7a360d44addd9e3def46f020d2951de4b7da1aac
Blackspot420 <ronnyyegon420@gmail.com>
2024-05-27 21:35:01 +0300

    Create global variable.cpp

---
+++ global variable.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+	
+	int x=5;//declaration of global variable 1
+   int y=6;//declaration of global variabe 2
+   
+   
+ printf("%d \n",x);//printing global variable x
+	
+	printf("%d",y);//printing global variable y
+	
+	
+	
+return 0;
