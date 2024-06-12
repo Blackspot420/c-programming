130bef15b5d722f2d82219a766a142034115b4ae
Blackspot420 <ronnyyegon420@gmail.com>
2024-06-12 08:55:44 +0300

    Create loan qualifications.cpp

---
+++ loan qualifications.cpp
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int age, income;
+    
+    printf("please enter your age: \n");
+    scanf("%d, age");
+    
+    printf("please enter your income: \n");
+    scanf("%d, income");
+    
+    if(age>=21 && income>=21000){
+    printf("you qualify for loan");
+    }else
+    printf("try next time");
+
+    return 0;
+}
