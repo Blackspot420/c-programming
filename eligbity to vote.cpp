ce7c23e22d2de67ff2e9cf6f6c71462f2219a18f
Blackspot420 <ronnyyegon420@gmail.com>
2024-06-12 09:06:40 +0300

    Create eligbity to vote.cpp

---
+++ eligbity to vote.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){ 
+    int age, kenya;
+    printf("please enter your age: \n");
+    scanf("%d, age");
+    
+    printf("please enter your nationality: \n");
+    scanf("%c, kenya");
+    
+    if(age>=18 && kenya){
+    printf("eligble to vote \n");
+    }else
+    printf("I'm sorry");
+    
+    
+    
+    
+     return 0;
+}
