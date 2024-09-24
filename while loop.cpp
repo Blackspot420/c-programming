417945b77481a6c3ea1ddc037fbf884eb762fa97
Blackspot420 <ronnyyegon420@gmail.com>
2024-09-24 21:57:03 +0300

    Create while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,16 @@
+// program to print the even numbers using whike loop
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int number = 2;
+    
+    while (number<=50) {
+        cout<<number<<endl;
+        number+=2;
+        
+    }
+    
+
+    return 0;
+}
